6c6fe9f329310d6eb831f72a4ba9126649904566
Alan Treanor <alan.treanor@ucdconnect.ie>
2019-02-21 08:30:52 +0000

    practical 01 - VolumeCalc

---
+++ src/practical_01/VolumeCalc.java
@@ -0,0 +1,21 @@
+package practical_01;
+// import Scanner class
+import java.util.Scanner;
+/*
+ * program reads radius & length of cyclinder from console & computes area
+ */
+public class VolumeCalc {
+	// main method
+	public static void main(String[] args) {
+		// create Scanner object
+		Scanner input = new Scanner(System.in);
+		
+		// prompt user for input
+		System.out.println("Enter radius & length of cyclinder separated by space: ");
+		double radius = input.nextDouble();
+		double length = input.nextDouble();
+		
+		
+	}
+
+}
