3f39b5f0a12cc2653d40c6d3cce8ba0ff75cb360
Alan Treanor <alan.treanor@ucdconnect.ie>
2019-03-01 08:13:26 +0000

    lecture 06 - testingCode

---
+++ src/lecture_06/testingCode.java
@@ -0,0 +1,12 @@
+package lecture_06;
+
+public class testingCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = (char)('0' + Math.random() * ('9' - '0' + 1));
+		System.out.println("Character: " + ch);
+		
+	}
+
+}
