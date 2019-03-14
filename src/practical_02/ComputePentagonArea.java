1199b9785846d16ab8665195ff60ae88f1766b4c
Alan Treanor <alan.treanor@ucdconnect.ie>
2019-03-14 11:20:15 +0000

    Practical 02 - Update ComputePentagonArea

--- src/practical_02/ComputePentagonArea.java
+++ src/practical_02/ComputePentagonArea.java
@@ -22,7 +22,7 @@ public class ComputePentagonArea {
 			double side = 2 * length * Math.sin(Math.PI / 5);
 			double area = 5 * Math.pow(side, 2) / (4 * Math.tan(Math.PI / 5));
 		
-			System.out.println("Area of pentagon: " + area);
+			System.out.println("Area of pentagon is: " + area);
 		}
 		else {
 			// print error message to console
